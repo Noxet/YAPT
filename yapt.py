b70ac1ef5e27b04589e16d9396f3c9411f7e4cce
Jonathan Sönnerup <Nooxet@gmail.com>
2019-01-01 18:58:40 +0100

    modified layout, testing colors and CSS on buttons, wip. Added button images

--- yapt.py
+++ yapt.py
@@ -11,6 +11,7 @@ import time
 
 BG_COLOR = '#555555'
 FG_COLOR = '#FFFFFF'
+MENU_BTN_WIDTH = 120
 
 class PicButton(QAbstractButton):
     def __init__(self, pixmap, pixmap_pressed, parent=None):
@@ -40,7 +41,7 @@ class PicButton(QAbstractButton):
         self.update()
 
     def sizeHint(self):
-        return QSize(325, 121)
+        return QSize(187, 86)
 
 
 class UARTThread(QThread):
@@ -88,22 +89,40 @@ class YAPTLayout(QWidget):
 
     def init_gui(self):
         #window.setFixedSize(500, 500)
-        self.setStyleSheet('background-color:{};color:{};font-family:Courier'.format(BG_COLOR, FG_COLOR))
+        #self.setStyleSheet('background-color:{};color:{};font-family:Courier'.format(BG_COLOR, FG_COLOR))
         self.layout = QGridLayout()
 
+        self.setStyleSheet("""
+            QPushButton {
+                border: 1px solid #121212;
+                border-radius: 2px;
+                background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,
+                                      stop: 0 #FB432c, stop: 1 #EF4923);
+                min-width: 80px;
+                min-height: 22px;
+            }
+
+            QPushButton#StyledButton {
+                color: #F00;
+                background-color: #000;
+            }
+                           """)
+
         # baud rate
-        baud_lab = QLabel('Select baud rate')
+        baud_lab = QLabel('Baud rate')
         self.layout.addWidget(baud_lab, 0, 0)
-        baud_btn = QPushButton('Baud rate')
-        baud_btn.setFixedWidth(160)
-        baud_btn.clicked.connect(self.select_baud)
-        self.layout.addWidget(baud_btn, 1, 0)
+        self.baud_btn = QPushButton('Baud rate')
+        #self.baud_btn.setStyleSheet('QPushButton {background-color:#EF4923}')
+        #self.baud_btn.setStyleSheet('QPushButton:pressed {background-color: qlineargradient(x1: 1, y1: 1, x2: 1, y2: 1, stop: 0 #EF4923, stop: 1 #EF4923, stop: 0 #EF4923, stop: 1 #EF4923)}')
+        self.baud_btn.setFixedWidth(MENU_BTN_WIDTH)
+        self.baud_btn.clicked.connect(self.select_baud)
+        self.layout.addWidget(self.baud_btn, 1, 0)
 
         # data bits
-        data_lab = QLabel('Select data bits')
+        data_lab = QLabel('Data bits')
         self.layout.addWidget(data_lab, 0, 1)
         data_cmb = QComboBox()
-        data_cmb.setFixedWidth(160)
+        data_cmb.setFixedWidth(MENU_BTN_WIDTH)
         data_cmb.addItem('5')
         data_cmb.addItem('6')
         data_cmb.addItem('7')
@@ -112,33 +131,81 @@ class YAPTLayout(QWidget):
         self.layout.addWidget(data_cmb, 1, 1)
 
         # stop bits
-        stop_lab = QLabel('Select stop bits')
+        stop_lab = QLabel('Stop bits')
         self.layout.addWidget(stop_lab, 0, 2)
         stop_cmb = QComboBox()
-        stop_cmb.setFixedWidth(160)
+        stop_cmb.setFixedWidth(MENU_BTN_WIDTH)
         stop_cmb.addItem('1')
         stop_cmb.addItem('2')
         self.layout.addWidget(stop_cmb, 1, 2)
+        
+        # parity
+        parity_lab = QLabel('Parity')
+        self.layout.addWidget(parity_lab, 0, 3)
+        parity_cmb = QComboBox()
+        parity_cmb.setFixedWidth(MENU_BTN_WIDTH)
+        parity_cmb.addItem('None')
+        parity_cmb.addItem('Even')
+        parity_cmb.addItem('Odd')
+        self.layout.addWidget(parity_cmb, 1, 3)
 
         # device port
-        dev_lab = QLabel('Select COM port')
-        self.layout.addWidget(dev_lab, 0, 3)
+        dev_lab = QLabel('COM port')
+        self.layout.addWidget(dev_lab, 0, 4)
         dev_cmb = QComboBox()
-        dev_cmb.setFixedWidth(160)
+        dev_cmb.setFixedWidth(MENU_BTN_WIDTH)
         dev_cmb.addItem('COM1')
         dev_cmb.addItem('COM3')
-        self.layout.addWidget(dev_cmb, 1, 3)
+        self.layout.addWidget(dev_cmb, 1, 4)
         
         # data format
-        format_lab = QLabel('Select data format')
-        self.layout.addWidget(format_lab, 0, 4)
+        format_lab = QLabel('Data format')
+        self.layout.addWidget(format_lab, 0, 5)
         format_cmb = QComboBox()
-        format_cmb.setFixedWidth(160)
+        format_cmb.setFixedWidth(MENU_BTN_WIDTH)
         format_cmb.addItem('Characters')
         format_cmb.addItem('Hex')
         format_cmb.addItem('Bin')
-        self.layout.addWidget(format_cmb, 1, 4)
+        self.layout.addWidget(format_cmb, 1, 5)
 
+        recv_lab = QLabel('Data received')
+        self.layout.addWidget(recv_lab, 2, 0)
+        recv_le = QLineEdit()
+        recv_le.setMinimumWidth(600)
+        recv_le.setMinimumHeight(600)
+        self.layout.addWidget(recv_le, 3, 0, 5, 5)
+
+        # send field and button
+        send_lab = QLabel('Data send')
+        self.layout.addWidget(send_lab, 9, 0)
+        send_le = QLineEdit()
+        send_le.setMinimumWidth(600)
+        self.layout.addWidget(send_le, 10, 0, 1, 5)
+ 
+        send_btn = PicButton(QPixmap('send_up.png'), QPixmap('send_down.png'))
+        send_btn.resize(187, 86)
+        self.layout.addWidget(send_btn, 10, 5)
+
+
+        conn = PicButton(QPixmap('connect_up.png'), QPixmap('connect_down.png'))#QPushButton('Connect')
+        conn.resize(187, 86)
+        #conn.setFixedWidth(MENU_BTN_WIDTH)
+        conn.clicked.connect(self.connect)
+        self.layout.addWidget(conn, 3, 5)
+
+        echo_cb = QCheckBox('Echo')
+        echo_cb.setChecked(True)
+        self.layout.addWidget(echo_cb, 4, 5)
+
+        color_cb = QCheckBox('Color')
+        color_cb.setChecked(True)
+        self.layout.addWidget(color_cb, 5, 5)
+
+
+#        send_btn = QPushButton('Send')
+#        top.setStyleSheet('background-color:#FF0000;color:#0000FF;')
+#        send_btn.clicked.connect(self.send)
+#        self.layout.addWidget(send_btn, 10, 7)
 
 #        baud_dd = QComboBox()
 #        baud_dd.addItem('9600')
@@ -146,14 +213,6 @@ class YAPTLayout(QWidget):
 #        self.layout.addWidget(baud_dd, 0, 0)
 #        baud_dd.activated[str].connect(self.alert)
 
-        self.send_text = QLineEdit()
-        self.layout.addWidget(self.send_text, 2, 0)
-        
-        self.recv_text = QLineEdit()
-        self.layout.addWidget(self.recv_text, 2, 1)
-        
-        self.baud = QLineEdit('9600')
-        self.layout.addWidget(self.baud)
 
 #        conn = PicButton(QPixmap('connect_up.png'), QPixmap('connect_down.png'))
         #conn.resize(325, 121)
@@ -161,10 +220,6 @@ class YAPTLayout(QWidget):
 #        self.layout.addWidget(conn)
       
 
-        self.top = QPushButton('Send')
-    #    top.setStyleSheet('background-color:#FF0000;color:#0000FF;')
-        self.layout.addWidget(self.top)
-        self.top.clicked.connect(self.send)
 
 
         self.setLayout(self.layout)
@@ -200,7 +255,13 @@ class YAPTLayout(QWidget):
         ret = diag.exec_()
 
         if ret == 1:
-            self.baud_rate = baud_rate.text()
+            try:
+                baud = int(baud_rate.text())
+            except:
+                self.alert('Baud rate must be an integer')
+                return
+            self.baud_rate = baud
+            self.baud_btn.setText(str(baud))
 
 
     def connect(self):
@@ -226,6 +287,8 @@ class YAPTLayout(QWidget):
 
     def alert(self, msg):
             alert = QMessageBox()
+            alert.setStyleSheet('background-color:{};color:{};'.format(BG_COLOR, FG_COLOR))
+            alert.setWindowTitle('Alert')
             alert.setText(msg)
             alert.exec_()
     
